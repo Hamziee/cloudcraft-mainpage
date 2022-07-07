24058cffe877ec687efbb4e1658e80f12aad77a0
Hamziee <91210085+Hamziee@users.noreply.github.com>
2022-07-07 15:10:44 +0200

    asd

---
+++ index.php
@@ -0,0 +1,43 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+		<meta name="viewport" content="width=device-width,initial-scale=1">
+		<meta name="author" content="zM4xi">
+		<meta name="keyword" content="minecraft theme, zm4xi">
+    <!-- Set the 3 to how long you want the page to "load" -->
+    <meta http-equiv="refresh" content="4;url=dashboard.php" />
+		<meta name="description" content="WelcomeToMinecraft is a OpenSource Website Theme Template">
+		<title>Welcome To Minecraft</title>
+
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/loader.css">
+    <link rel="stylesheet" href="css/wtm.css">
+    <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+  </head>
+
+  <body>
+
+    <!-- loader start -->
+    <div id="loader-wrapper">
+        <div id="loader"></div>
+
+        <div class="loader-section section-left"></div>
+        <div class="loader-section section-right"></div>
+
+    </div>
+    <!-- loader end -->
+
+    <!-- content start -->
+    <div id="content">
+
+    </div>
+    <!-- content end -->
+
+  </body>
+
+
+  <script src="js/main.js"></script>
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+  <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
+
+</html>
